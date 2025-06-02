97a4b488173eb3002dd7fa76cde1bcab9f20cb75
Fariha Promi <farihapromi2611@gmail.com>
2025-06-02 22:41:07 +0600

    Add navabr

---
+++ learn-react-query/src/components/RQSuperherosPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RQSuperherosPage = () => {
+  return (
+    <div>
+      <h3>RQ</h3>
+    </div>
+  );
+};
+
+export default RQSuperherosPage;
---
+++ learn-react-query/src/components/SuperherosPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SuperherosPage = () => {
+  return (
+    <div>
+      <h1>Super</h1>
+    </div>
+  );
+};
+
+export default SuperherosPage;
